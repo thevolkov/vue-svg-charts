2cbb6ac8d0bd95aae6176c92342d34b264abd670
volkovigor <info@viil.ru>
2023-09-23 03:08:41 +0500

    Create labeled input component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <LabeledInput
+      :input-label="'Value'"
+      :input-type="'text'"
+      v-model="inputValue"
+    />
+    {{ inputValue }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import LabeledInput from '@/components/LabeledInput.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  components: { LabeledInput },
+  data() {
+    return {
+      inputValue: 50,
+    };
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LabeledInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <label :for="lowerInputLabel">
+    {{ inputLabel }}
+    <input
+      @input="updateValue"
+      :value="value"
+      :name="lowerInputLabel"
+      :type="inputType"
+      :min="min"
+      :max="max"
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'LabeledInput',
+  props: [
+    'inputLabel',
+    'inputType',
+    'value',
+    'min',
+    'max',
+  ],
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+  computed: {
+    lowerInputLabel() {
+      return this.inputLabel.toLowerCase();
+    },
+  },
+};
+</script>
